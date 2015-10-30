e757d976891556ee9b754c1ff55dc56b04582e33
OdetteDu <Odette.Du@rice.edu>
2015-10-29 17:37:39 -0700

    generate head word map

--- src/cs224n/corefsystems/BetterBaseline.java
+++ src/cs224n/corefsystems/BetterBaseline.java
@@ -1,8 +1,6 @@
 package cs224n.corefsystems;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 
 import cs224n.coref.ClusteredMention;
@@ -11,11 +9,66 @@ import cs224n.coref.*;
 import cs224n.util.Pair;
 
 public class BetterBaseline implements CoreferenceSystem {
+	
+	private Map<String, Set<String>> headWordMap;
+	
+	private void addToHeadWordMap(String word1, String word2)
+	{
+		if (headWordMap == null)
+		{
+			headWordMap = new HashMap<String, Set<String>>();
+		}
+		
+		if (headWordMap.containsKey(word1))
+		{
+			headWordMap.get(word1).add(word2);
+		}
+		else
+		{
+			Set<String> temp = new HashSet<String>();
+			temp.add(word2);
+			headWordMap.put(word1, temp);
+		}
+		
+		if (headWordMap.containsKey(word2))
+		{
+			headWordMap.get(word2).add(word1);
+		}
+		else
+		{
+			Set<String> temp = new HashSet<String>();
+			temp.add(word1);
+			headWordMap.put(word2, temp);
+		}
+	}
 
 	@Override
 	public void train(Collection<Pair<Document, List<Entity>>> trainingData) {
-		// TODO Auto-generated method stub
-
+		
+		for (Pair<Document, List<Entity>> pair : trainingData)
+		{
+			for (Entity entity : pair.getSecond())
+			{
+				Set<String> currentSet = new HashSet<String>();
+				for (Mention m : entity.mentions)
+				{
+					String headWord = m.headWord();
+					if (!Pronoun.isSomePronoun(headWord))
+					{
+						currentSet.add(headWord);
+					}
+				}
+				String[] temp = new String[currentSet.size()];
+				currentSet.toArray(temp);
+				for(int i=0; i<temp.length-1; i++)
+				{
+					for(int j=i+1; j<temp.length; j++)
+					{
+						this.addToHeadWordMap(temp[i], temp[j]);
+					}
+				}
+			}
+		}
 	}
 
 	@Override
